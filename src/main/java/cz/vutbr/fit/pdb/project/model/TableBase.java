148a9171a4a197a479c4362322a619a5187f8891
vprusa <vprusa@redhat.com>
2017-12-04 09:19:24 +0100

    Refactored Zona class to use static crud methods and inheritance of TableBase to use persistant connection for all tables

--- src/main/java/cz/vutbr/fit/pdb/project/model/TableBase.java
+++ src/main/java/cz/vutbr/fit/pdb/project/model/TableBase.java
@@ -1,5 +1,13 @@
 package cz.vutbr.fit.pdb.project.model;
 
-public interface TableBase {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TableBase {
+
+	protected static final String PERSISTENCE_UNIT_NAME = "project01";
+	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	protected static EntityManager entityManager = emf.createEntityManager();
 
 }
